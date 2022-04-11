b14a25427a4da6090e1ce7a51e7c0d201d829e71
Lloyds01 <soloyede708@gmail.com>
2022-04-11 06:52:36 -0700

    UPDATE [setup postgres database]

--- Liberty_Pay/settings.py
+++ Liberty_Pay/settings.py
@@ -76,12 +76,29 @@ WSGI_APPLICATION = 'Liberty_Pay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# if DEBUG:
+#     # demo database
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'personal_loan_db',
+            'USER': 'postgres',
+            'PASSWORD': 'Forlan123',
+            'HOST': 'localhost',
+            'PORT': "5432",
+            
+        }
+
     }
-}
 
 
 # Password validation
